7f2ab5c155539d9e10f563cea3c4668d64947e21
felixcm1129 <felixcm1129@hotmail.ca>
2020-04-14 22:12:41 -0400

    3.3.3 complete

---
+++ BillingManagement.Business/InvoicesDataService .cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using app_models;
+using BillingManagement.Models;
+
+namespace BillingManagement.Business
+{
+    class InvoicesDataService : IDataService <Invoice>
+    {
+        List<Invoice> invoices;
+        List<Customer> _customers;
+
+        public IEnumerable<Invoice> GetAll()
+        {
+            foreach (Invoice i in invoices)
+            {
+                yield return i;
+            }
+        }
+
+        public InvoicesDataService()
+        {
+            initValues();
+        }
+
+        private void initValues()
+        {
+            Random rnd = new Random();
+
+
+            foreach (var customer in _customers)
+            {
+                int nbInvoices = rnd.Next(10);
+
+                for (int i = 0; i < nbInvoices; i++)
+                {
+                    var invoice = new Invoice(customer);
+                    invoice.SubTotal = rnd.NextDouble() * 100 + 50;
+                    customer.Invoices.Add(invoice);
+                    invoices.Add(invoice);
+                }
+            }
+        }
+    }
+}
